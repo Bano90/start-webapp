cd5e07a820ed6f0cafc32f28ac46912348dedfd0
Bano90 <albanopeter1990@gmail.com>
2019-03-11 19:09:14 +0100

    ok

--- app/components/dashboard/dashboard.ctrl.js
+++ app/components/dashboard/dashboard.ctrl.js
@@ -5,8 +5,8 @@
       .module('app')
       .controller('DashboardController', DashboardController);
   
-    DashboardController.$inject = ['$state', '$rootScope', '$filter'];
-    function DashboardController($state, $rootScope, $filter) {
+    DashboardController.$inject = ['$state', '$rootScope', '$filter','DashboardService'];
+    function DashboardController($state, $rootScope, $filter,DashboardService) {
       var vm = this;
   
       activate();
